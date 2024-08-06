53eb356985af1febc437204f52bc13dbd276dc30
Hugo <hugo.saraiva.nascimento@gmail.com>
2024-08-06 17:41:10 -0300

    Fixing panic when file does not exist

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,16 @@
-use std::path::PathBuf;
+use std::path::{self};
 
 pub struct Settings {
     pub location: String,
     pub database: DatabaseSettings,
 }
-
 impl Settings {
     pub fn new(location: String) -> Self {
-        let path = PathBuf::from(location)
-            .canonicalize()
-            .unwrap()
+        let path = path::absolute(location)
+            .expect("Invalid file location")
             .into_os_string()
             .into_string()
-            .unwrap();
+            .expect("Failed to convert location to absolute path");
         Self {
             database: DatabaseSettings::new(&path),
             location: path,
