b633a2fbb69aaf7151512f9c88a84a0ed00030c2
Hugo <hugo.saraiva.nascimento@gmail.com>
2024-08-14 02:22:41 -0300

    Adding db to local storage folder

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,31 +1,14 @@
-use std::path::{self};
+use std::path::PathBuf;
 
 pub struct Settings {
-    pub location: String,
-    pub database: DatabaseSettings,
+    pub location: PathBuf,
 }
 impl Settings {
-    pub fn new(location: String) -> Self {
-        let path = path::absolute(location)
-            .expect("Invalid file location")
-            .into_os_string()
-            .into_string()
-            .expect("Failed to convert location to absolute path");
-        Self {
-            database: DatabaseSettings::new(&path),
-            location: path,
-        }
+    pub fn new(location: PathBuf) -> Self {
+        Self { location }
     }
 }
 
 pub struct DatabaseSettings {
     pub location: String,
-}
-
-impl DatabaseSettings {
-    pub fn new(location: &str) -> Self {
-        Self {
-            location: format!("sqlite:{location}"),
-        }
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@ pub mod domain;
 pub mod generator;
 pub mod scopes;
 pub mod startup;
+pub mod storage;
 pub mod tasks;
